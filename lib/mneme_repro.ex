a24c7550de68ba5cfe32c072dcc8721b077f2918
Jason Axelson <jason.axelson@gmail.com>
2023-11-12 08:17:28 -1000

    Initial commit

---
+++ lib/mneme_repro.ex
@@ -0,0 +1,18 @@
+defmodule MnemeRepro do
+  @moduledoc """
+  Documentation for `MnemeRepro`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MnemeRepro.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/mneme_repro_test.exs
@@ -0,0 +1,8 @@
+defmodule MnemeReproTest do
+  use ExUnit.Case
+  doctest MnemeRepro
+
+  test "greets the world" do
+    assert MnemeRepro.hello() == :world
+  end
+end
